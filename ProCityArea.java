f4195d25899c2b7d3a540302efafb2574190c082
2424693824ls <2424693824@qq.com>
2019-07-19 14:39:09 +0800

    sdfsdfds

--- ProCityArea.java
+++ ProCityArea.java
@@ -9,24 +9,24 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
 public class ProCityArea {
 
-
-    /**
-     * http://www.mca.gov.cn/article/sj/xzqh/2019/
-     * notepad，去除左右空格，tab转空格
-     * @param args
-     * @throws Exception
-     */
     public static void main(String[] args) throws Exception {
         List<Map<String, Object>> list = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader("d:\\省市区.txt"));
+
         String str = "";
         while ((str = br.readLine()) != null) {
-            String[] split = str.split("  ");
+
+            Iterable<String> iterable = Splitter.on(" ").trimResults().omitEmptyStrings().split(str);
+            ArrayList<String> list2 = Lists.newArrayList(iterable);
+
             Map<String, Object> map = new HashMap<>();
-            map.put("code", split[0]);
-            map.put("name", split[1]);
+            map.put("code", list2.get(0));
+            map.put("name", list2.get(1));
             list.add(map);
         }
         br.close();
