fd9eef95bdb8ed0632761806488a09c2fade84bf
2424693824ls <2424693824@qq.com>
2019-07-24 09:41:38 +0800

    adfdsfsdfds

--- ProCityArea.java
+++ ProCityArea.java
@@ -1,4 +1,4 @@
-package com.yucong.excel;
+package com.yucong.util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.google.common.base.Splitter;
@@ -14,14 +13,16 @@ import com.google.common.collect.Lists;
 
 public class ProCityArea {
 
-	//http://www.mca.gov.cn/article/sj/xzqh/2019/
+    /**
+     * <li>http://www.mca.gov.cn/article/sj/xzqh/2019/</li>
+     * <li>notepad++，去除左右空格，tab改为空格</li>
+     */
     public static void main(String[] args) throws Exception {
         List<Map<String, Object>> list = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader("d:\\省市区.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("d:\\aaaa.txt"));
 
         String str = "";
         while ((str = br.readLine()) != null) {
-
             Iterable<String> iterable = Splitter.on(" ").trimResults().omitEmptyStrings().split(str);
             ArrayList<String> list2 = Lists.newArrayList(iterable);
 
@@ -31,11 +32,60 @@ public class ProCityArea {
             list.add(map);
         }
         br.close();
-        System.out.println("数据总数：" + list.size());
 
-        Set<String> set = list.stream().map(m -> m.get("code").toString().substring(0, 2)).distinct().collect(Collectors.toSet());
+        // 省
+//        List<String> list2 = list.stream().map(m -> m.get("code").toString().substring(0, 2)).distinct().collect(Collectors.toList());
+//        List<Map<String, Object>> list4 = list.stream().filter(m -> m.get("code").toString().endsWith("0000")).collect(Collectors.toList());
+//
+//        System.out.println(list2.size());
+//        System.out.println(list4.size());
+//
+//        // INSERT into vwt_dictionary VALUES('151', 'code', '行政区划', 'name', 'pid', 'soldier_info');
+//
+//        for (Map<String, Object> map : list4) {
+//            System.out.println("INSERT into vwt_dictionary VALUES('151', '" + map.get("code").toString().substring(0, 2) + "', '行政区划', '" + map.get("name")
+//                    + "', '-1', 'soldier_info';");
+//        }
+        
+        
 
-        System.out.println("省份数量：" + set.size());
+        // 市, 110000 北京市, 120000 天津市, 310000 上海市, 500000 重庆市
+//        List<Map<String, Object>> list5 = list.stream().filter(m -> !m.get("code").toString().endsWith("0000")).collect(Collectors.toList());
+//        List<Map<String, Object>> list6 = list5.stream().filter(m -> m.get("code").toString().startsWith("11") || m.get("code").toString().startsWith("12")
+//                || m.get("code").toString().startsWith("31") || m.get("code").toString().startsWith("50")).collect(Collectors.toList());
+//        System.out.println(list6.size());
+//
+//        List<Map<String, Object>> list7 = list5.stream().filter(m -> m.get("code").toString().endsWith("00")).collect(Collectors.toList());
+//        System.out.println(list7.size());
+//
+//        for (Map<String, Object> map : list6) {
+//            String substring = map.get("code").toString().substring(0, 2);
+//            System.out.println("INSERT into vwt_dictionary VALUES('151', '" + map.get("code") + "', '行政区划', '" + map.get("name") + "', '" + substring
+//                    + "', 'soldier_info';");
+//        }
+//        for (Map<String, Object> map : list7) {
+//            String substring = map.get("code").toString().substring(0, 2);
+//            System.out.println("INSERT into vwt_dictionary VALUES('151', '" + map.get("code") + "', '行政区划', '" + map.get("name") + "', '" + substring
+//                    + "', 'soldier_info';");
+//        }
+
+
+        // 区县
+        List<Map<String, Object>> list8 = list.stream().filter(m -> !m.get("code").toString().endsWith("00")).collect(Collectors.toList());
+        List<Map<String, Object>> list9 = list8.stream().filter(m -> !m.get("code").toString().startsWith("11") && !m.get("code").toString().startsWith("12")
+                && !m.get("code").toString().startsWith("31") && !m.get("code").toString().startsWith("50")).collect(Collectors.toList());
+
+        int count = 0;
+        for (Map<String, Object> map : list9) {
+            count++;
+            if (count % 500 == 0) {
+                Thread.sleep(20000);
+            }
+            String substring = map.get("code").toString().substring(0, 4);
+            System.out.println("INSERT into vwt_dictionary VALUES('151', '" + map.get("code") + "', '行政区划', '" + map.get("name") + "', '" + substring
+                    + "', 'soldier_info';");
+        }
+
     }
 
 }
